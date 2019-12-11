0a5d0af5d00210453d677786490e7d58e3eb4455
Chucks Nwachukwu <chucks@bbd.co.za>
2019-12-11 17:14:03 +0200

    Using events module

--- using-events/index.js
+++ using-events/index.js
@@ -7,13 +7,64 @@ class MyEmitter extends EventEmitter {
 
 const myEmitter = new MyEmitter();
 
-// Event emitters exposes the on method used to name an event and listener to be called when the event occures
+// Event emitters exposes the on method used to name an event and a listener to be called when the event occures
 myEmitter.on('myEvent', () => {
     console.log('An Event has occured');
 });
 
-// Firing a named event
+// triggering a named event
 setTimeout(() => {
   myEmitter.emit('myEvent');
+}, 2000);
+
+/**
+ * Passing argument to listeners 
+ */
+myEmitter.on('morning', function(date, month) {
+  console.log('Time: %s', date);
+  console.log('Month: %s', month);
+  // The 'this' keyword is itentionally set to refernce the EventEmitter instance 
+  console.log(this === myEmitter); // true
+  console.log(this instanceof EventEmitter); //true
+});
+
+myEmitter.on('morning', () => {
+  // For ES6 error function, this does not reference the EventEmitter instance
+  console.log(this === myEmitter); //
+  console.log(this instanceof EventEmitter); // false
+});
+
+setTimeout(() => {
+  const date = new Date();
+  myEmitter.emit('morning', date.getDate(), date.getMonth());
 }, 3000);
+
+/** 
+ *  EventEmitter calls all listeners synchronously in the order in which they were registered.  
+ * We switch listener functions to asynchronous mode of operation using the setImediate() or process.nextTick() method.
+ */
+class WorkEmitter extends EventEmitter {
+
+}
+const workEmitter = new WorkEmitter();
+
+workEmitter.on('work', function(job) {
+    console.log('Doing the %s work 1', job);
+});
+workEmitter.on('work', function(job) {
+  setImmediate(() => {
+    console.log('Doing the %s work 2', job)
+  });
+
+});
+workEmitter.on('work', function(job) {
+  console.log('Doing the %s work 3', job);
+});
+
+workEmitter.emit('work', 'Microservices');
+
+/**
+ * Handling events only once
+ */
+
 
--- using-events/readme.md
+++ using-events/readme.md
@@ -1,3 +1,6 @@
 # EventEmitter 
 [Docs](https://nodejs.org/api/events.html)
 The `events` native module holds the `EventEmitter` class. 
+All objects that emit events are instances of the EventEmitter class.  
+When the EventEmitter object emits and vent, all of the functions attached to that specific events are called synchronously. 
+Any value returned by the called listeners are ignored and will be discarded.
